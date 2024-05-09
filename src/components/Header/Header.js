9b5750e8d2bd16bdba8884661b90169e172dd9e7
TinhPN-Donga <tinhpn@donga.edu.vn>
2024-05-09 10:21:05 +0000

    header continue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,31 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-
-import { logoPNG } from '../../utils/picture'
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
+import { Avatar, Box, ButtonBase } from '@mui/material';
+import { logoPNG } from '../../utils/picture';
+import { SVGAttributes } from 'react'
 function ResponsiveAppBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
     return (
         <Box sx={{width: 1, display: 'flex'}} >
-            <Box sx={{ width: '260px', backgroundColor: 'blue', p:2}}>
-                <img src={logoPNG.logoDongA} alt='logo' width='50%'/>
+            <Box sx={{ width: '280px',  p:2,pt:1,pb:1, display: 'flex', justifyContent: 'space-between'}}>
+            <Box sx={{ width: '55%'}}>
+                <ButtonBase disableRipple to='dashboard'><img src={logoPNG.logoDongA} alt='logo' width='100%' /></ButtonBase>
+            </Box>
+            <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
+                    <Avatar
+                        variant="rounded"
+                        sx={{
+                            transition: 'all .2s ease-in-out',
+                            background: 'white',
+                            color: 'gray',
+                            '&:hover': {
+                                background: 'gray',
+                                color: 'white'
+                            }
+                        }}
+                        color="inherit"
+                    >
+                    </Avatar>
+                </ButtonBase>
             </Box>
             <Box sx={{ width: '100%', backgroundColor: 'red' }}>Item 2</Box>
         </Box>
